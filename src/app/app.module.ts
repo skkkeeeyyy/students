bff9e3434de2d5dc7094c8e9de4ea9e573f4d060
skkkyyyeee <anoopru2002@gmail.com>
2022-07-07 20:37:53 +0530

    students ui done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsCollegeComponent } from './students-college/students-college.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsCollegeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/students-college/students-college.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-students-college',
+  templateUrl: './students-college.component.html',
+  styleUrls: ['./students-college.component.css']
+})
+export class StudentsCollegeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
